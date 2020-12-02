e1c7699095e278e3e2f1562094cbb82ab4fe717b
Cynthia Peter <cynthiapeterpearl@gmail.com>
2020-12-02 12:41:04 +0100

    spalsh screen set

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.white,
+            body: SafeArea(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Mai Blog',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                    ),
+                  ],
+                ),
+              ),
+            )));
+  }
+}
